8198c5b7637d6c67be7ca5c39450bb8e115b06b9
Chinmay Pandhari <pandharichinmay@gmail.com>
2021-05-20 10:21:48 +0530

    Commit Employee Management

---
+++ src/components/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <h1>About Us: </h1><hr />
+        <p className="lead">
+        Learn and create successfully one RPA project for a US client. Create the web pages using bootstrap. Creating APIs using NODE.js,Express.js using PostgreSQL database and call them from angular.Reso.
+Creating web pages and android apps using HTML, CSS, Bootstrap, and android studio.</p>   
+<p className="lead"> Create backend using spring boot java and node express with database MySQL and SQL. I have a total of 2 years of experience as a Java Full Stack Developer.
+Technologies you know : Frontend : Html,css,bootstrap,android studio; Backend:Java spring boot,Node,Express;Database:SQL,MySQL,PostgreSQL;Other:RPA,Blockchain.</p>
+
+            </div>
+    </div>
+  );
+};
+
+export default About;
